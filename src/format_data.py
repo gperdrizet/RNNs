f1107c8d20e41d8359705fab42b8b063765420fe
George Perdrizet <george@perdrizet.org>
2026-02-13 00:14:16 +0000

    Finished alphabet and sentiment analysis demo

--- src/format_data.py
+++ src/format_data.py
@@ -1,19 +1,28 @@
 '''Parse and format raw data.'''
 
+# Standard library
+from pathlib import Path
+
+# Third-party
 import pandas as pd
+
 
 if __name__ == '__main__':
 
+    # Set data path relative to this file's location
+    DATA_DIR = Path(__file__).resolve().parent.parent / 'data'
+    RAW_DATA_DIR = DATA_DIR / 'raw_data'
+
     # Load the raw data
-    datasets = [pd.read_csv('../data/raw_data/twitter-2016dev-CE.txt', sep='\t')]
-    datasets.append(pd.read_csv('../data/raw_data/twitter-2016devtest-CE.txt', sep='\t'))
-    datasets.append(pd.read_csv('../data/raw_data/twitter-2016test-CE.txt', sep='\t'))
-    datasets.append(pd.read_csv('../data/raw_data/twitter-2016train-CE.txt', sep='\t'))
+    column_names = ['id', 'name', 'score', 'text']
+    datasets = [pd.read_csv(RAW_DATA_DIR / 'twitter-2016dev-CE.txt', sep='\t', header=None, names=column_names)]
+    datasets.append(pd.read_csv(RAW_DATA_DIR / 'twitter-2016devtest-CE.txt', sep='\t', header=None, names=column_names))
+    datasets.append(pd.read_csv(RAW_DATA_DIR / 'twitter-2016test-CE.txt', sep='\t', header=None, names=column_names))
+    datasets.append(pd.read_csv(RAW_DATA_DIR / 'twitter-2016train-CE.txt', sep='\t', header=None, names=column_names))
 
     # Concatenate the datasets
-    df = pd.concat(datasets, ignore_index=True)
+    df = pd.concat(datasets, axis=0, ignore_index=True)
     print(df.head())
 
     # Save the formatted data
-    df.to_parquet('../data//twitter-2016.parquet', index=False)
-    
+    df.to_parquet(DATA_DIR / 'twitter-2016.parquet', index=False)
